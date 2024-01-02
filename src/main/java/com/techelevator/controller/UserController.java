be959f3e042dcab68e039c69f0f4ae7a9b6a1e13
Tom Beerbower <thomas@techelevator.com>
2024-01-02 10:09:14 -0500

    rework validation of user game guesses

--- src/main/java/com/techelevator/controller/UserController.java
+++ src/main/java/com/techelevator/controller/UserController.java
@@ -128,14 +128,4 @@ public class UserController {
         }
         gameService.updateUserGame(userId, gameId, userGame);
     }
-
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            errors.put(((FieldError) error).getField(), error.getDefaultMessage());
-        });
-        return errors;
-    }
 }
